13b5bfd478a04d590f86a131b1acb1ba115c48a8
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-04 13:55:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ from pydantic import BaseModel
 app = FastAPI(root_path="/api")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("ALLOWED_ORIGINS", "https://askneta.com,http://localhost:5174, http://localhost:5173").split(","),
+    allow_origins=("http://localhost:5173", "http://localhost:5174", "https://askneta.com").split(","),
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
